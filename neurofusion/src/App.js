4bae0f52022cd745b22a1c6a83272c7fa235a020
Ore Ogundipe <oreogundipe@gmail.com>
2022-10-05 11:51:38 -0700

    let's get this bread

---
+++ neurofusion/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import fusionlogo from './fusion-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={fusionlogo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to neurofusion! It's all conencted :)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
