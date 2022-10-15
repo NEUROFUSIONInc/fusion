02c049d45e15c232bd42885318539592cb3d2799
Ore Ogundipe <oreogundipe@gmail.com>
2022-10-14 20:24:15 -0700

    add signal quality dump to ui ✨

--- neurofusion/client/src/routes/root.jsx
+++ neurofusion/client/src/routes/root.jsx
@@ -10,10 +10,16 @@ export default function Root() {
 
     const [notion, setNotion] = useState(null);
     const [loggedIn, setLoggedIn] = useState(false);
+    // const [signalQualitySeries, setSignalQualitySeries] = useState({});
+    const [signalQualityValues, setSignalQualityValues] = useState({});
+    
+    // e.g CP3, C3, F5, PO3, PO4, F6, C4, CP4
+    const [channelNames, setChannelNames] = useState([]);
     
     useEffect(() => {
         // it all starts with having a device id set
         // (you can save it in local storage after setting it once)
+        console.log(deviceId)
         if (deviceId) {
             const notion_instance = new Notion({ deviceId });
             setNotion(notion_instance);
@@ -30,36 +36,62 @@ export default function Root() {
         login()
 
         async function login(){
-            const auth = await notion
+            await notion
             .login({
                 email,
                 password
             }).then(() => {
-                alert("logged in to neurosity")
+                console.log("connected to neurosity")
                 setLoggedIn(true)
             }).catch((error) => {
-                alert("failed to login")
+                console("failed to connect to neurosity")
+                console.log(error)
             })
 
-            if(auth) {
-                setLoggedIn(true)
-            }
         }
 
     }, [notion]);
 
     useEffect(() => {
-        if(loggedIn) {
-            // we can now show the user a button to start an experiment
-        }
-    }, [loggedIn])
+        console.log(loggedIn)
+        if(loggedIn && notion) {
 
-    function getSignalQuality(){
-        // live feed of signal quality
-    }
+            (async () => {
+                const channelNames = (await notion.getInfo()).channelNames;
+                setChannelNames(channelNames);
+            })();
+        }
+
+    }, [loggedIn, notion])
     
     function genericExperiment(){
         // record raw data
+        console.log("getting signal quality")
+        
+        // live feed of signal quality
+        notion.signalQuality().subscribe(signalQuality => {
+            
+            let signalQualityEntry = {
+                'unixTimestamp_secs': Math.floor(Date.now() / 1000),
+            }
+
+            // change value after every 10 seconds
+            if( signalQualityEntry.unixTimestamp_secs % 5 == 0) {
+                console.log("formatting signal quality entry");
+                
+                for (let ch_index = 0; ch_index < channelNames.length; ch_index++) {
+
+                    let ch_name = channelNames[ch_index];
+                    signalQualityEntry[ch_name + "_value"] = signalQuality[ch_index].standardDeviation;
+                    signalQualityEntry[ch_name + "_status"] = signalQuality[ch_index].status;
+    
+                }
+
+                console.log("updating signal quality average")
+                setSignalQualityValues(signalQualityEntry)
+                console.log(signalQualityEntry)
+            }
+        });
     }
     
     return (
@@ -71,9 +103,15 @@ export default function Root() {
             <p>..for the curious</p>
         </div>
 
+        <div id="devices">
+            {/* select box to choose device */}
+        </div>
+
         { loggedIn ? (
             <div id="signalquality">
-
+                <h2>Signal Quality</h2>
+                {/* chart for data quality ranges */}
+                <p>Signal quality is {JSON.stringify(signalQualityValues)}</p>
             </div>
         ) :
             <></>
