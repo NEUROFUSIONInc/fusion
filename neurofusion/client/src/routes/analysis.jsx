e0696bf414321189eef78a1799e95f405ebc0c8e
Ore Ogundipe <oreogundipe@gmail.com>
2022-12-07 05:19:22 +0000

    static data display for focus predictions & work context

--- neurofusion/client/src/routes/analysis.jsx
+++ neurofusion/client/src/routes/analysis.jsx
@@ -2,11 +2,66 @@ import React, { useState, useEffect } from 'react';
 import ReactEcharts from "echarts-for-react";
 import SideNavBar from '../components/sidenavbar';
 
+import magicFlowContexts from "../assets/magicflow_contexts_clean.json"
+import neurosityFocus from "../assets/neurosity_focus_clean.json"
 
 export default function Analysis() {
 
+    const seriesData = neurosityFocus.map((item) => {
+        return [item.timestamp, item.probability]
+    })
+
+    const verticalLinesData = magicFlowContexts.map((item) => {
+        return {
+            name: item.categories[0],
+            xAxis: item.timestamp,
+            lineStyle: {
+                type: 'solid',
+                color: 'green'
+            }
+        }
+    })
+
+
+    const options = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross'
+            }
+        },
+        xAxis: {
+            type: 'time',
+            splitLine: {
+                show: true,
+            }
+        },
+        yAxis: {
+            type: 'value',
+            name: 'focus probability',
+            splitLine: {
+                show: true
+            }
+        },
+        series: [
+            {
+                type: 'line',
+                smooth: true,
+                data: seriesData,
+                markLine: {
+                    label: {
+                        show: true,
+                        formatter: '{b}'
+                    },
+                    symbol: 'none',
+                    data: verticalLinesData
+                }
+            }
+        ]
+    };
+
     // for now just feed in the base data
-    return(
+    return (
         <>
             <SideNavBar></SideNavBar>
 
@@ -14,6 +69,10 @@ export default function Analysis() {
                 marginLeft: '10%',
             }}>
                 <p>Correlating your brain activity with health & productivity signals</p>
+
+                <ReactEcharts
+                    option={options}
+                />
             </main>
 
         </>
