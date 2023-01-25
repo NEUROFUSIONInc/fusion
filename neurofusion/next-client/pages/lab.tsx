d2aaf61af590b3be3828944c655c14edf96a6ddb
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-25 23:31:12 +0000

    chore: add protected routes only accessible when user is logged in

---
+++ neurofusion/next-client/pages/lab.tsx
@@ -0,0 +1,26 @@
+import { GetServerSideProps, NextPage } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
+
+const LabPage: NextPage = () => {
+  return <div>Protected page</div>;
+};
+
+export default LabPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
