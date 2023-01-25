3d8144299de464d4a711e2bdc9fb1c2dafb8ab75
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-25 23:33:27 +0000

    feat: create default index view

---
+++ neurofusion/next-client/pages/index.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const { data: session, status } = useSession();
+  return (
+    <div className="p-2">
+      <Image
+        src="/images/logo.png"
+        alt="Neurofusion Logo"
+        width={80}
+        height={80}
+      />
+      {session ? (
+        <>
+          <h1>Welcome {session.user?.email}</h1>
+          <button
+            onClick={() => signOut()}
+            className="py-1 px-4 bg-gray-900 text-zinc-50 rounded-md"
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <Link href="/auth/login">Login</Link>
+      )}
+      <Link href="/lab" className="block">
+        Go to protected lab route
+      </Link>
+
+      <Link href="/recordings" className="block">
+        Go to another protected route (Recordings)
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
