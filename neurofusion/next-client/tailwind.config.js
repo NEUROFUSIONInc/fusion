20f05db30420f5fe429006bba92c3e70ff1c9c91
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-27 20:18:27 +0000

    chore: update paths in config files

--- neurofusion/next-client/tailwind.config.js
+++ neurofusion/next-client/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
