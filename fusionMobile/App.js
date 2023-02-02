1690614ea0da80849111568b86326892c15defb1
Ore Ogundipe <oreogundipe@gmail.com>
2023-02-01 19:28:56 -0800

    init mobile project

---
+++ fusionMobile/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Add logo here */}
+      <Text>Welcome to Fusion</Text>
+      <Text></Text>
+
+      {/* Add button to get started or just show them */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
