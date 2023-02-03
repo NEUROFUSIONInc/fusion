d0f1e0e3cfc44ae9c8a542d6e1eae01a1e4193e0
Kelechiodom10 <kelechiodom10@gmail.com>
2023-02-03 14:20:05 +0000

    chore: update login page with newly created components

--- neurofusion/next-client/src/components/ui/index.ts
+++ neurofusion/next-client/src/components/ui/index.ts
@@ -1,2 +1,6 @@
 export * from "./button/button";
 export * from "./logo/logo";
+export * from "./layouts";
+export * from "./auth";
+export * from "./link/button-link";
+export * from "./input/input";
--- neurofusion/next-client/src/pages/auth/login.tsx
+++ neurofusion/next-client/src/pages/auth/login.tsx
@@ -1,23 +1,18 @@
 import { GetServerSideProps } from "next";
 import { unstable_getServerSession } from "next-auth";
-import { Magic } from "magic-sdk";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useForm } from "react-hook-form";
 
 import { authOptions } from "../api/auth/[...nextauth]";
 
-const magic = typeof window !== "undefined" && new Magic(process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY || "a");
-
-interface FormValue {
-  email: string;
-}
+import { MainLayout } from "~/components/layouts";
+import { LoginContainer } from "~/components/ui";
+import { magic } from "~/lib/magic";
 
 const LoginPage = () => {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<FormValue>();
 
-  const onSubmit = async ({ email }: FormValue) => {
+  const onSubmit = async (email: string) => {
     if (!magic) throw new Error(`magic not defined`);
 
     const didToken = await magic.auth.loginWithMagicLink({ email });
@@ -30,18 +25,11 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register("email", { required: true })}
-          className="border"
-          placeholder="valid email address"
-          autoComplete="email"
-        />
-
-        <button type="submit">Sign in</button>
-      </form>
-    </div>
+    <MainLayout>
+      <div className="mx-auto mt-16 flex w-full justify-center">
+        <LoginContainer onSubmit={onSubmit} />
+      </div>
+    </MainLayout>
   );
 };
 
