b25f984c7cd10df3d1ae934321f2bec8b3b21182
Ore Ogundipe <oreogundipe@gmail.com>
2023-02-10 12:36:02 -0800

    heartbea

--- fusionMobile/App.js
+++ fusionMobile/App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* Add logo here */}
       <Text>Welcome to Fusion</Text>
-      <Text></Text>
+
+      <Text>
+        Core concepts is that you get reminders about events and you respond
+      </Text>
+
+      {/* Would you like us to prompt you at intervals to tell us what you're doing? */}
+
+      {/* question, cadence, input */}
 
       {/* Add button to get started or just show them */}
       <StatusBar style="auto" />
@@ -17,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
