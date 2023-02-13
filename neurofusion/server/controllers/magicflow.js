42ca1c4988d10e7b9487187a5c91c356d0b14183
Ore Ogundipe <oreogundipe@gmail.com>
2023-02-13 14:01:39 -0800

    update magicflow to use UserProvider class

--- neurofusion/server/controllers/magicflow.js
+++ neurofusion/server/controllers/magicflow.js
@@ -1,28 +1,23 @@
-const db = require('../models/index');
-const { magicFlowQueue, storageQueue } = require('../queue');
-
+const db = require("../models/index");
+const { magicFlowQueue, storageQueue } = require("../queue");
 
 exports.fetchToken = async (req, res) => {
   try {
-    const userMetadata = await db.UserMetadata.findOne({ where: { userEmail: req.user.userEmail } });
+    const userProvider = await fetchUserProvider(
+      req.user.userGuid,
+      "Magicflow"
+    );
 
-    if (!userMetadata) {
-      return res.status(401)
-        .json({
-          error: "User does not exist"
-        });
-    }
-
-    res.status(200)
-      .json({
-        magicflowToken: userMetadata.magicflowToken
+    if (userProvider) {
+      res.status(200).json({
+        magicflowToken: userProvider.providerToken,
       });
+    }
   } catch (err) {
     console.error(err);
-    res.status(400)
-      .json({
-        error: "Unable to fetch magicflow token"
-      });
+    res.status(400).json({
+      error: "Unable to fetch magicflow token",
+    });
   }
 };
 
@@ -30,36 +25,62 @@ exports.setToken = async (req, res) => {
   const magicflowToken = req.body.magicflowToken;
 
   if (!magicflowToken) {
-    return res.status(400)
-      .json({
-        error: "Invalid payload."
-      });
+    return res.status(400).json({
+      error: "Invalid payload.",
+    });
   }
 
   try {
-    await db.UserMetadata.update({ magicflowToken: magicflowToken }, {
-      where: {
-        userGuid: req.user.userGuid
-      }
-    });
+    const userProvider = await fetchUserProvider(
+      req.user.userGuid,
+      "Magicflow"
+    );
+
+    if (!userProvider) {
+      throw new Error("Magicflow provider does not exist");
+    }
+
+    userProvider.providerToken = magicflowToken;
+    await userProvider.save();
 
     // Fetch user's magicflow data in the background
     magicFlowQueue.push({
       guid: req.user.userGuid,
       token: magicflowToken,
-      lastFetched: req.user.magicflowLastFetched,
-      storageQueue // Importing this from magicflow processor wasn't working so passing the instance
+      lastFetched: userProvider.providerLastFetched,
+      storageQueue, // Importing this from magicflow processor wasn't working so passing the instance
     });
 
-    res.status(200)
-      .json({
-        magicflowToken
-      });
+    res.status(200).json({
+      magicflowToken,
+    });
   } catch (err) {
     console.error(err);
-    res.status(400)
-      .json({
-        error: "Unable to update magicflow token"
-      });
+    res.status(400).json({
+      error: "Unable to update magicflow token",
+    });
   }
 };
+
+async function fetchUserProvider(userGuid, providerName) {
+  // get the magicflow provider guid
+  // TODO: load this object on app startup and store it in memory
+  const magicflowProvider = await db.Provider.findOne({
+    where: { name: providerName },
+  });
+
+  if (!magicflowProvider) {
+    console.log("Magicflow provider does not exist");
+    return null;
+  }
+
+  // check if user has magicflow token
+  const userProvider = await db.UserProvider.findOne({
+    where: {
+      userGuid: userGuid,
+      providerGuid: magicflowProvider.guid,
+    },
+  });
+
+  return userProvider;
+}
