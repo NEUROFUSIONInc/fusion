bbe4dffa0ac5447c0e93197859fee699cfca2171
Kelechiodom10 <kelechiodom10@gmail.com>
2023-02-27 17:53:33 +0000

    chore: add margin to login container

--- neurofusion/next-client/src/components/ui/auth/login-container/login-container.tsx
+++ neurofusion/next-client/src/components/ui/auth/login-container/login-container.tsx
@@ -10,7 +10,7 @@ interface LoginContainerProps {
 
 export const LoginContainer: FC<LoginContainerProps> = ({ onSubmit }) => {
   return (
-    <div className="flex w-96 max-w-sm flex-col items-center space-y-6 rounded-md border bg-white py-12 px-4 shadow-md dark:border-secondary-400 dark:border-opacity-50 dark:bg-transparent dark:shadow-sm dark:shadow-gray-700">
+    <div className="m-4 flex w-96 max-w-sm flex-col items-center space-y-6 rounded-md border bg-white py-12 px-4 shadow-md dark:border-secondary-400 dark:border-opacity-50 dark:bg-transparent dark:shadow-sm dark:shadow-gray-700">
       <Logo className="w-16" />
       <h1 className="text-2xl font-bold">Login to Fusion</h1>
       <LoginForm onSubmit={onSubmit} />
