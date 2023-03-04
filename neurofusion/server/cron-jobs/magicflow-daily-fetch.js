b368856aa11a63ead6c4e2bf032639dca39b0f4e
Emmanuel Akinpelu <hemor2709@gmail.com>
2023-03-03 21:16:45 -0800

    Updated magicflow cron job to log errors

--- neurofusion/server/cron-jobs/magicflow-daily-fetch.js
+++ neurofusion/server/cron-jobs/magicflow-daily-fetch.js
@@ -18,28 +18,32 @@ const { magicFlowQueue, storageQueue } = require('../queue');
 const CRON_EXPRESSION = "0 0 0 * * *"; // 12:00 AM every day
 
 const job = async () => {
-  console.log(`MAGICFLOW_DAILY_FETCH: running on ${dayjs().toString()}`);
-  const users = await db.UserMetadata.findAll({
-    where: {
-      magicflowToken: {
-        [Op.not]: null
-      },
-      magicflowLastFetched: {
-        [Op.or]: {
-          [Op.lt]: dayjs().startOf('day'),
-          [Op.eq]: null
+  try {
+    console.log(`MAGICFLOW_DAILY_FETCH: running on ${dayjs().toString()}`);
+    const users = await db.UserMetadata.findAll({
+      where: {
+        magicflowToken: {
+          [Op.not]: null
+        },
+        magicflowLastFetched: {
+          [Op.or]: {
+            [Op.lt]: dayjs().startOf('day'),
+            [Op.eq]: null
+          }
         }
       }
-    }
-  });
-  console.log(`MAGICFLOW_DAILY_FETCH: updating magicflow data for ${users.length} users`);
-  for (const user of users) {
-    magicFlowQueue.push({
-      guid: user.userGuid,
-      token: user.magicflowToken,
-      lastFetched: user.magicflowLastFetched,
-      storageQueue // Importing this from magicflow processor wasn't working so passing the instance
     });
+    console.log(`MAGICFLOW_DAILY_FETCH: updating magicflow data for ${users.length} users`);
+    for (const user of users) {
+      magicFlowQueue.push({
+        guid: user.userGuid,
+        token: user.magicflowToken,
+        lastFetched: user.magicflowLastFetched,
+        storageQueue // Importing this from magicflow processor wasn't working so passing the instance
+      });
+    }
+  } catch (err) {
+    console.error(err);
   }
 };
 
