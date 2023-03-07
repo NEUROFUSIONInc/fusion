94c7a77838e293d1c4f41da5a8fbc871b43235d5
Kelechiodom10 <kelechiodom10@gmail.com>
2023-03-07 19:05:28 +0000

    feat: create reusable avatar component using radix ui avatar component

---
+++ neurofusion/next-client/src/components/ui/avatar/avatar.tsx
@@ -0,0 +1,40 @@
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import classNames from "classnames";
+import { forwardRef } from "react";
+
+const Avatar = forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={classNames("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
+    {...props}
+  />
+));
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image ref={ref} className={classNames("aspect-square h-full w-full", className)} {...props} />
+));
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={classNames(
+      "flex h-full w-full items-center justify-center rounded-full border-slate-200 bg-primary-900 font-bold text-white dark:border-slate-700 dark:bg-white dark:text-slate-800",
+      className
+    )}
+    {...props}
+  />
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarImage, AvatarFallback };
