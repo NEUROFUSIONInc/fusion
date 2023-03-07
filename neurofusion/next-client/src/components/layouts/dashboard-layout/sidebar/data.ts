7d94d9ebb7083c67f7475d68527eefdcf929dbfe
Kelechiodom10 <kelechiodom10@gmail.com>
2023-03-07 19:12:25 +0000

    feat: create sidebar component that houses logged in nav links

---
+++ neurofusion/next-client/src/components/layouts/dashboard-layout/sidebar/data.ts
@@ -0,0 +1,24 @@
+import { HardDrive, LayoutDashboard, Plug, SignalHigh } from "lucide-react";
+
+export const sidebarLinks = [
+  {
+    icon: LayoutDashboard,
+    title: "Dashboard",
+    href: "#",
+  },
+  {
+    icon: Plug,
+    title: "Integrations",
+    href: "#",
+  },
+  {
+    icon: HardDrive,
+    title: "Datasets",
+    href: "#",
+  },
+  {
+    icon: SignalHigh,
+    title: "Analysis",
+    href: "#",
+  },
+];
