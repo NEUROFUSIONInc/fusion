797478c0b931e8949ba5d07bd29b993fef1693f4
Ore Ogundipe <oreogundipe@gmail.com>
2023-04-03 16:05:21 -0700

    chore: running https locally

---
+++ neurofusion/next-client/server.js
@@ -0,0 +1,25 @@
+const https = require("https");
+const fs = require("fs");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync("./certs/localhost-key.pem"),
+  cert: fs.readFileSync("./certs/localhost.pem"),
+};
+
+app.prepare().then(() => {
+  https
+    .createServer(httpsOptions, (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    })
+    .listen(3000, (err) => {
+      if (err) throw err;
+      console.log("> Ready on https://localhost:3000");
+    });
+});
