9e580c86e9b35b533f331be09ffffb8c34a6f303
Kelechi Odom <kelechiodom10@gmail.com>
2023-05-02 17:51:16 +0100

    Add dropdown menu for user account and update frontend dependencies (#80)
    * chore: update styling for dark variant button
    
    * chore: update imports and code cleanliness
    
    * feat: show logged in user in sidebar footer
    
    * feat: set up neurosity state and ability for user to integrate neurosity
    
    * chore: add neurosity callback screen that logs user in
    
    * feat: add neurosity modal that allows user to disconnect or choose devices
    
    * feat: install neurosity sdk and update https config instructions
    
    * chore: update README
    
    * chore: update redirect url
    
    * fix: fix eslint errors
    
    * chore: update frontend next client dependencies
    
    * chore: setup neurofusion server locally
    
    * feat: install dependencies for setting up dropdown menu
    
    * chore: update imports for logout button and dropdown menu
    
    * create logout button
    
    * feat: add account menu to sidebar footer
    
    * feat: show logged in user in sidebar footer
    
    * feat: install neurosity sdk and update https config instructions
    
    * chore: update frontend next client dependencies
    
    * chore: setup neurofusion server locally
    
    * feat: install dependencies for setting up dropdown menu
    
    * chore: update imports for logout button and dropdown menu
    
    * create logout button
    
    * feat: add account menu to sidebar footer
    
    * chore: update cross origin google font

--- neurofusion/next-client/src/components/layouts/dashboard-layout/sidebar/sidebar-footer.tsx
+++ neurofusion/next-client/src/components/layouts/dashboard-layout/sidebar/sidebar-footer.tsx
@@ -1,11 +1,11 @@
-import { ChevronsUpDown } from "lucide-react";
 import { User } from "next-auth";
 import { useTheme } from "next-themes";
 import { FC, useEffect, useState } from "react";
 
+import { AccountMenu } from "./account-menu";
 import { appearanceModes } from "./data";
 
-import { Avatar, AvatarFallback, AvatarImage, Tabs, TabsList, TabsTrigger } from "~/components/ui";
+import { Tabs, TabsList, TabsTrigger } from "~/components/ui";
 
 interface ISidebarFooterProps {
   user?: User;
@@ -22,23 +22,7 @@ export const SidebarFooter: FC<ISidebarFooterProps> = ({ user }) => {
 
   return (
     <div className="mx-auto flex w-full flex-col items-stretch space-y-5 rounded-none md:w-64 lg:w-72">
-      <div className="group relative flex w-full items-center justify-between rounded-md border p-2 hover:cursor-pointer hover:bg-slate-50 dark:border-slate-700/60 dark:hover:border-transparent dark:hover:bg-slate-800">
-        <div className="flex items-center space-x-4">
-          <Avatar>
-            {user?.image && <AvatarImage src={user?.image} alt={user?.name || "User Avatar"} />}
-            <AvatarFallback>{user?.name?.substring(0, 1).toUpperCase()}</AvatarFallback>
-          </Avatar>
-          <div className="flex-flex-col">
-            <p className="w-[90%] truncate text-[14px] font-medium">{user?.name?.split("@")[0]}</p>
-            <p className=" max-w-[120px] truncate text-[12.5px] text-slate-600 dark:text-slate-400 lg:max-w-[160px]">
-              {user?.email}
-            </p>
-          </div>
-        </div>
-        <div className="absolute top-auto right-2">
-          <ChevronsUpDown size={18} />
-        </div>
-      </div>
+      <AccountMenu user={user} />
       <Tabs defaultValue={theme} className="w-full">
         <TabsList className="w-full">
           {appearanceModes.map((mode) => (
@@ -46,7 +30,7 @@ export const SidebarFooter: FC<ISidebarFooterProps> = ({ user }) => {
               key={mode.value}
               value={mode.value}
               title={`${mode.value} mode`}
-              className="min-w-[80px] cursor-pointer sm:min-w-[82px]"
+              className="sm:min-w-[82px] min-w-[80px] cursor-pointer"
               onMouseDown={() => setTheme(mode.value)}
             >
               <mode.icon size={20} />
