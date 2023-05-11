d3179ff2cb83c1d0c5fcf38b61e711c4b6966939
Kelechiodom10 <kelechiodom10@gmail.com>
2023-05-11 23:50:41 +0100

    feat: create reusable hook for using prompt context

---
+++ fusionMobile/src/hooks/usePrompts.ts
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { PromptContext } from "../utils";
+
+export const usePrompts = () => {
+  const promptContext = useContext(PromptContext);
+
+  if (!promptContext) {
+    throw new Error(
+      "useCurrentUser has to be used within <PromptContext.Provider>"
+    );
+  }
+
+  return promptContext;
+};
