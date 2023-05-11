2730bb2bb341c617889ada9acd5edbdbda8ec892
Kelechiodom10 <kelechiodom10@gmail.com>
2023-05-11 23:59:51 +0100

    chore: convert prompt entry screen to ts

---
+++ fusionMobile/src/pages/promptEntry.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export function PromptEntryScreen() {
+  return (
+    <View>
+      <Text>Hey there, you're currently using Fusion anonymously!</Text>
+    </View>
+  );
+}
