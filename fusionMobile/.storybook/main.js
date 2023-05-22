72622ba511b327c55145426ae677152defdd31ff
Kelechiodom10 <kelechiodom10@gmail.com>
2023-05-21 21:01:24 -0400

    feat: install and setup storybook to build ui components

---
+++ fusionMobile/.storybook/main.js
@@ -0,0 +1,10 @@
+// .storybook/main.js
+module.exports = {
+  stories: ["../src/components/**/*.stories.?(ts|tsx|js|jsx)"],
+  addons: [
+    "@storybook/addon-ondevice-controls",
+    "@storybook/addon-ondevice-actions",
+    "@storybook/addon-ondevice-backgrounds",
+  ],
+  framework: "@storybook/react-native",
+};
