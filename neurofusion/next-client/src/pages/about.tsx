6c086c13fef2c7d5cfb4241e60df017b45e7fe84
Ore Ogundipe <oreogundipe@gmail.com>
2023-05-27 16:00:06 -0400

    fix build

--- neurofusion/next-client/src/pages/about.tsx
+++ neurofusion/next-client/src/pages/about.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 
-// import { FeatureSection, HeroSection, IntegrationsSection, Newsletter } from "~/components/features/landing";
 import { MainLayout } from "~/components/layouts";
 
 const About: NextPage = () => {
@@ -9,7 +8,7 @@ const About: NextPage = () => {
     window.location.href = "https://rainy-oak-138.notion.site/NEUROFUSION-Team-39887d6b988c4c83ae2a16ae8db44a45";
   }, []);
 
-  return <MainLayout></MainLayout>;
+  return <MainLayout />;
 };
 
 (About as any).theme = "dark";
