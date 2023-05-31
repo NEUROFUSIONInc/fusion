6624e5a36938fdec366acd177939adea0e9469f9
Kelechiodom10 <kelechiodom10@gmail.com>
2023-05-31 16:08:32 +0100

    chore: set up custom font loader to show splash screen while fonts load

---
+++ fusionMobile/FontLoader.tsx
@@ -0,0 +1,46 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import React, { PropsWithChildren, useCallback } from "react";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+interface Props extends PropsWithChildren {
+  onFontsLoaded?: () => void; // callback for displaying the splash screen once background view has loaded
+}
+
+SplashScreen.preventAutoHideAsync();
+
+export const FontLoader = ({ onFontsLoaded, children }: Props) => {
+  const insets = useSafeAreaInsets();
+  const [fontsLoaded] = useFonts({
+    "wsh-thin": require("./assets/fonts/GTWalsheimPro-Thin.ttf"),
+    "wsh-light": require("./assets/fonts/GTWalsheimPro-Light.ttf"),
+    "wsh-reg": require("./assets/fonts/GTWalsheimPro-Regular.ttf"),
+    "wsh-med": require("./assets/fonts/GTWalsheimPro-Medium.ttf"),
+    "wsh-semi": require("./assets/fonts/GTWalsheimPro-Bold.ttf"),
+    "wsh-bold": require("./assets/fonts/GTWalsheimPro-Black.ttf"),
+    "wsh-xbold": require("./assets/fonts/GTWalsheimPro-UltraBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync(); //hide the splashscreen
+    }
+  }, [fontsLoaded, onFontsLoaded]);
+
+  if (!fontsLoaded) {
+    return <View />;
+  }
+
+  return (
+    <View
+      style={{
+        paddingTop: insets.top,
+        flex: 1,
+      }}
+      onLayout={onLayoutRootView}
+    >
+      {children}
+    </View>
+  );
+};
