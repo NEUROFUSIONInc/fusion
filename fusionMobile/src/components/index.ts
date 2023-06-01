da1349bbe1875738dff5deb0b7af1e09765b6aae
Kelechiodom10 <kelechiodom10@gmail.com>
2023-06-01 22:07:49 +0100

    export all custom design components

--- fusionMobile/src/components/index.ts
+++ fusionMobile/src/components/index.ts
@@ -1,1 +1,7 @@
 export * from "./button";
+export * from "./tag";
+export * from "./day-chip";
+export * from "./select";
+export * from "./timepicker";
+export * from "./option";
+export * from "./icons";
