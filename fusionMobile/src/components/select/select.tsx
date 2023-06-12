020464b0b45e51547825ed8643fcfbd58a6ffb76
Kelechiodom10 <kelechiodom10@gmail.com>
2023-06-12 21:26:19 +0100

    feat: add label to select component

--- fusionMobile/src/components/select/select.tsx
+++ fusionMobile/src/components/select/select.tsx
@@ -1,14 +1,18 @@
 import { Entypo } from "@expo/vector-icons";
 import React, { FC, useState } from "react";
+import { Text } from "react-native";
 import DropDownPicker, {
   DropDownPickerProps,
   ValueType,
 } from "react-native-dropdown-picker";
 
+import colors from "~/theme/colors";
+
 type PickerProps = Omit<
   DropDownPickerProps<ValueType>,
   "open" | "setOpen" | "multiple"
 > & {
+  label?: string;
   multiple?: boolean;
 };
 
@@ -19,88 +23,96 @@ export const Select: FC<PickerProps> = ({
   placeholder = "Select an option",
   searchPlaceholder = "Search...",
   multiple = false,
+  label,
   ...props
 }) => {
-  const light = "rgba(255, 255, 255, 0.50)";
   const separatorColor = "rgba(255, 255, 255, 0.07)";
   const [open, setOpen] = useState(false);
   const [dropdownItems, setDropdownItems] = useState(items);
 
   return (
-    <DropDownPicker
-      value={value}
-      items={dropdownItems}
-      placeholder={placeholder}
-      searchPlaceholder={searchPlaceholder}
-      // @ts-ignore
-      setValue={setValue as (value: ValueType | ValueType[] | null) => void}
-      setItems={setDropdownItems}
-      itemSeparator
-      containerStyle={{
-        height: 52,
-        borderColor: "#E5E5E5",
-        backgroundColor: "transparent",
-      }}
-      placeholderStyle={{
-        color: light,
-        fontFamily: "wsh-reg",
-        fontSize: 16,
-      }}
-      ArrowUpIconComponent={() => (
-        <Entypo name="chevron-small-up" size={18} color={light} />
+    <>
+      {label && (
+        <Text className="text-white text-base font-sans mb-3">{label}</Text>
       )}
-      ArrowDownIconComponent={() => (
-        <Entypo name="chevron-small-down" size={18} color={light} />
-      )}
-      dropDownContainerStyle={{
-        backgroundColor: "#23212D",
-        borderWidth: 0,
-        marginTop: 18,
-        width: "100%",
-        borderRadius: 6,
-        zIndex: 100,
-      }}
-      listItemLabelStyle={{
-        color: "white",
-        fontFamily: "wsh-reg",
-        fontSize: 16,
-      }}
-      searchTextInputStyle={{
-        color: "white",
-        fontFamily: "wsh-reg",
-        fontSize: 16,
-        borderColor: separatorColor,
-        height: 40,
-      }}
-      searchContainerStyle={{
-        borderBottomColor: separatorColor,
-      }}
-      searchTextInputProps={{
-        placeholderTextColor: "white",
-      }}
-      TickIconComponent={() => <Entypo name="check" size={18} color="white" />}
-      listItemContainerStyle={{
-        height: 49,
-      }}
-      props={{
-        activeOpacity: 0.8,
-      }}
-      labelStyle={{
-        color: "white",
-        fontFamily: "wsh-reg",
-        fontSize: 16,
-      }}
-      style={{
-        borderColor: light,
-        backgroundColor: "transparent",
-      }}
-      itemSeparatorStyle={{
-        backgroundColor: separatorColor,
-      }}
-      multiple={multiple as true}
-      {...props}
-      open={open}
-      setOpen={setOpen}
-    />
+      <DropDownPicker
+        value={value}
+        items={dropdownItems}
+        placeholder={placeholder}
+        searchPlaceholder={searchPlaceholder}
+        // @ts-ignore
+        setValue={setValue as (value: ValueType | ValueType[] | null) => void}
+        setItems={setDropdownItems}
+        itemSeparator
+        containerStyle={{
+          height: 52,
+          borderColor: "#E5E5E5",
+          backgroundColor: "transparent",
+        }}
+        placeholderStyle={{
+          color: colors.light,
+          fontFamily: "wsh-reg",
+          fontSize: 16,
+        }}
+        ArrowUpIconComponent={() => (
+          <Entypo name="chevron-small-up" size={18} color={colors.light} />
+        )}
+        ArrowDownIconComponent={() => (
+          <Entypo name="chevron-small-down" size={18} color={colors.light} />
+        )}
+        dropDownContainerStyle={{
+          backgroundColor: "#23212D",
+          borderColor: colors.light,
+          borderWidth: 1,
+          marginTop: 16,
+          width: "100%",
+          borderRadius: 6,
+          zIndex: 100,
+        }}
+        listItemLabelStyle={{
+          color: "white",
+          fontFamily: "wsh-reg",
+          fontSize: 16,
+        }}
+        searchTextInputStyle={{
+          color: "white",
+          fontFamily: "wsh-reg",
+          fontSize: 16,
+          borderColor: separatorColor,
+          height: 40,
+        }}
+        searchContainerStyle={{
+          borderBottomColor: separatorColor,
+        }}
+        searchTextInputProps={{
+          placeholderTextColor: "white",
+        }}
+        TickIconComponent={() => (
+          <Entypo name="check" size={18} color="white" />
+        )}
+        listItemContainerStyle={{
+          height: 48,
+        }}
+        props={{
+          activeOpacity: 0.8,
+        }}
+        labelStyle={{
+          color: "white",
+          fontFamily: "wsh-reg",
+          fontSize: 16,
+        }}
+        style={{
+          borderColor: colors.light,
+          backgroundColor: "transparent",
+        }}
+        itemSeparatorStyle={{
+          backgroundColor: separatorColor,
+        }}
+        multiple={multiple as true}
+        {...props}
+        open={open}
+        setOpen={setOpen}
+      />
+    </>
   );
 };
