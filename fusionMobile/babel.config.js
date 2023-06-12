057ec90654675aa1a0f649f02a1bcecf01b8e3c8
Kelechiodom10 <kelechiodom10@gmail.com>
2023-06-12 21:10:46 +0100

    feat: install bottom sheet and portal packages

--- fusionMobile/babel.config.js
+++ fusionMobile/babel.config.js
@@ -12,6 +12,12 @@ module.exports = function (api) {
         },
       ],
       "nativewind/babel",
+      [
+        "react-native-reanimated/plugin",
+        {
+          relativeSourceLocation: true,
+        },
+      ],
     ],
   };
 };
