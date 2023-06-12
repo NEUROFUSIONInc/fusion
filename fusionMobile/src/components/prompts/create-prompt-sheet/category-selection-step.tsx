86b08a523f705bc8c9b3d8a725c4a6d5442ad114
Kelechiodom10 <kelechiodom10@gmail.com>
2023-06-12 21:30:24 +0100

    feat: create category selection step component of creating a prompt feature

---
+++ fusionMobile/src/components/prompts/create-prompt-sheet/category-selection-step.tsx
@@ -0,0 +1,53 @@
+import { Dispatch, FC, SetStateAction } from "react";
+import { Text, View } from "react-native";
+
+import { Tag } from "../../tag";
+
+const categories = [
+  "Health and Fitness",
+  "Productivity",
+  "Finance",
+  "Education",
+  "Relationships",
+  "Sustainability",
+  "Mental Health",
+  "Spiritual Practice",
+];
+
+interface CategorySelectionStepProps {
+  selectedCategory: string;
+  setSelectedCategory: Dispatch<SetStateAction<string>>;
+}
+
+export const CategorySelectionStep: FC<CategorySelectionStepProps> = ({
+  setSelectedCategory,
+  selectedCategory,
+}) => {
+  return (
+    <View className="flex flex-1 flex-col items-start justify-center">
+      <View className="flex-col gap-y-3">
+        <Text className="text-xl font-sans-bold text-white">
+          Select a category
+        </Text>
+        <Text className="text-base font-sans text-gray-400">
+          Choose a category that best describes your prompt.
+        </Text>
+      </View>
+
+      <View className="flex flex-row gap-2.5 space-y-2.5 flex-wrap py-5">
+        {categories.map((category) => {
+          const isActive = category === selectedCategory;
+
+          return (
+            <Tag
+              key={category}
+              title={category}
+              isActive={isActive}
+              onPress={() => setSelectedCategory(category)}
+            />
+          );
+        })}
+      </View>
+    </View>
+  );
+};
