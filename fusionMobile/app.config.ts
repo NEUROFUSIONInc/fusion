77bf7299e732f1469c246ebe5951c448e97608fd
Kelechiodom10 <kelechiodom10@gmail.com>
2023-06-12 21:28:57 +0100

    feat: convert app config to typescript and add custom colors to splash screen

--- fusionMobile/app.config.ts
+++ fusionMobile/app.config.ts
@@ -1,14 +1,18 @@
-module.exports = {
+import type { ConfigContext, ExpoConfig } from "@expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
   name: "Fusion",
   slug: "fusion",
   version: "1.0.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
+  backgroundColor: "#0B0816",
   splash: {
     image: "./assets/splash.png",
     resizeMode: "contain",
-    backgroundColor: "#ffffff",
+    backgroundColor: "#0B0816",
   },
   updates: {
     fallbackToCacheTimeout: 0,
@@ -18,11 +22,12 @@ module.exports = {
     supportsTablet: true,
     bundleIdentifier: "com.neurofusion.fusion",
     buildNumber: "14",
+    backgroundColor: "#0B0816",
   },
   android: {
     adaptiveIcon: {
       foregroundImage: "./assets/icon.png",
-      backgroundColor: "#FFFFFF",
+      backgroundColor: "#0B0816",
     },
     package: "com.neurofusion.fusion",
     versionCode: 7,
@@ -39,4 +44,4 @@ module.exports = {
   },
   plugins: ["expo-notifications"],
   owner: "oreogundipe",
-};
+});
