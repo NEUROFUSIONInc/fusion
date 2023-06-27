d6ce5fabd273fcaafa6d2a34dd59548daf326b57
Ore Ogundipe <oreogundipe@gmail.com>
2023-06-27 22:35:30 +0100

    build: bump versions for release!!!!!

--- fusionMobile/app.config.ts
+++ fusionMobile/app.config.ts
@@ -20,7 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.neurofusion.fusion",
-    buildNumber: "22",
+    buildNumber: "23",
     backgroundColor: "#0B0816",
   },
   android: {
@@ -29,7 +29,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#0B0816",
     },
     package: "com.neurofusion.fusion",
-    versionCode: 14,
+    versionCode: 15,
   },
   web: {
     favicon: "./assets/favicon.png",
