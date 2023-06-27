bb9c268846b2c6ebebf85e9341c16c59e4b2d769
Kelechiodom10 <kelechiodom10@gmail.com>
2023-06-27 18:33:13 +0100

    feat: add optional icon prop to prompt option component

--- fusionMobile/src/components/prompts/prompt-option/prompt-option.tsx
+++ fusionMobile/src/components/prompts/prompt-option/prompt-option.tsx
@@ -1,19 +1,27 @@
 import { FC } from "react";
-import { Pressable, PressableProps, Text } from "react-native";
+import { Pressable, PressableProps, Text, View } from "react-native";
 
 import { ChevronRight } from "../../icons";
 
 interface PromptOptionProps extends PressableProps {
   text: string;
+  icon?: React.ReactElement;
 }
 
-export const PromptOption: FC<PromptOptionProps> = ({ text, ...props }) => {
+export const PromptOption: FC<PromptOptionProps> = ({
+  text,
+  icon,
+  ...props
+}) => {
   return (
     <Pressable
       className="bg-tint rounded-md w-full h-14 px-4 flex flex-row items-center justify-between active:opacity-80"
       {...props}
     >
-      <Text className="font-sans text-base text-white">{text}</Text>
+      <View className="flex flex-row items-center gap-x-2">
+        {icon && icon}
+        <Text className="font-sans text-base text-white">{text}</Text>
+      </View>
       <ChevronRight />
     </Pressable>
   );
