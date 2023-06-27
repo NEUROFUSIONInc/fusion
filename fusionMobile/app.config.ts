a73b59d27d1c4457df0fbb30d7fa987c0877cfe6
Ore Ogundipe <oreogundipe@gmail.com>
2023-06-28 00:44:51 +0100

    buildL update pkg

--- fusionMobile/app.config.ts
+++ fusionMobile/app.config.ts
@@ -20,7 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.neurofusion.fusion",
-    buildNumber: "26",
+    buildNumber: "27",
     backgroundColor: "#0B0816",
   },
   android: {
@@ -29,7 +29,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#0B0816",
     },
     package: "com.neurofusion.fusion",
-    versionCode: 17,
+    versionCode: 18,
   },
   web: {
     favicon: "./assets/favicon.png",
