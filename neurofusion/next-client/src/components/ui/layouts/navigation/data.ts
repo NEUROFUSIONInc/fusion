a2a9553c3d6d11b0910fc653b53c6fd2ecdc8717
Ore O <oreogundipe@gmail.com>
2023-07-01 18:45:37 +0100

    fix: hide research tab

--- neurofusion/next-client/src/components/ui/layouts/navigation/data.ts
+++ neurofusion/next-client/src/components/ui/layouts/navigation/data.ts
@@ -11,8 +11,8 @@ export const navigationLinks = [
     title: "Contact",
     href: "mailto:ore@usefusion.app",
   },
-  {
-    title: "Research",
-    href: "https://usefusion.app/research",
-  },
+  // {
+  //   title: "Research",
+  //   href: "https://usefusion.app/research",
+  // },
 ];
