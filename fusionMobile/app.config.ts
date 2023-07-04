18c2eb894c909d543fcc4f2e82ae8d4ecfd19c26
Ore Ogundipe <oreogundipe@gmail.com>
2023-07-04 11:10:56 +0100

    feat: bump package

--- fusionMobile/app.config.ts
+++ fusionMobile/app.config.ts
@@ -20,7 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.neurofusion.fusion",
-    buildNumber: "36",
+    buildNumber: "37",
     backgroundColor: "#0B0816",
   },
   android: {
@@ -29,7 +29,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#0B0816",
     },
     package: "com.neurofusion.fusion",
-    versionCode: 26,
+    versionCode: 27,
     softwareKeyboardLayoutMode: "pan",
   },
   web: {
