2c020d95b370bda1ce6bec48f49200e52d9ead9f
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-20 20:35:16 +0100

    feat: create generic prompt example component shown in feature section

---
+++ neurofusion/next-client/src/components/features/landing/feature-section/prompt-example/prompt-example.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode } from "react";
+import { cn } from "~/utils";
+
+interface PromptExampleProps {
+  title: ReactNode;
+  leftSubtitle: string;
+  rightSubtitle: string;
+  className?: string;
+}
+
+export const PromptExample: FC<PromptExampleProps> = ({ title, leftSubtitle, rightSubtitle, className }) => {
+  return (
+    <div
+      className={cn(
+        "w-64 px-6 py-6 bg-white rounded-bl-lg rounded-br-lg shadow border-t-2 border-indigo-700 flex-col justify-start items-start gap-4 flex",
+        className
+      )}
+    >
+      <div className="w-36 text-base font-normal leading-normal">{title}</div>
+      <div className="justify-start text-gray-500 items-center gap-[9px] inline-flex">
+        <p className=" text-sm font-normal leading-normal">{leftSubtitle}</p>
+        <div className="w-1.5 h-1.5 bg-gray-500 rounded-full" />
+        <p className="text-sm font-normal leading-normal">{rightSubtitle}</p>
+      </div>
+    </div>
+  );
+};
