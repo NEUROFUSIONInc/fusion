fcd84cdcc52c59ae94501c17471893970841f0ee
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-20 20:40:12 +0100

    feat: update footer to match designs

--- neurofusion/next-client/src/components/ui/layouts/footer/footer.tsx
+++ neurofusion/next-client/src/components/ui/layouts/footer/footer.tsx
@@ -4,31 +4,39 @@ import React from "react";
 import { Logo } from "../../logo/logo";
 
 import { footerLinks } from "./data";
+import { MsPartner } from "./ms-partner";
+import { Newsletter } from "./newsletter/newsletter";
 
 export const Footer = () => {
   return (
-    <footer className="border-t border-t-gray-200 dark:border-t-gray-800">
+    <footer className="border-t border-t-gray-200 bg-gray-100/90 dark:border-t-gray-800">
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="container mx-auto flex flex-col items-center overflow-hidden px-4 py-8 sm:px-6 lg:px-8">
-        <Logo />
-        <nav className="my-6 flex  flex-wrap justify-center" aria-label="Footer">
-          {footerLinks.map((link) => (
-            <Link
-              key={link.title}
-              href={`${link.href}`}
-              className="ml-8 text-[14.5px] font-medium text-primary-800 opacity-90 last:mr-8 hover:text-primary-900 hover:opacity-100 dark:text-primary-50 dark:hover:text-white"
-            >
-              {link.title}
-            </Link>
-          ))}
-        </nav>
-        <p className="text-center">
-          <span className="mx-auto mt-2 text-[14.5px] text-gray-600 dark:text-gray-400">
-            © NEUROFUSION Research Inc. All rights reserved. {new Date().getFullYear()}
-          </span>
-        </p>
+      <div className="container mx-auto flex flex-col items-center space-y-6 overflow-hidden px-4 pt-16 sm:px-6 lg:px-8">
+        <Logo withText />
+        <Newsletter />
+        <MsPartner />
+
+        <div className="flex w-full flex-col-reverse items-center justify-between border-t border-t-gray-200 pb-4 md:flex-row">
+          <p className="text-center">
+            <span className="mx-auto mt-2 text-[15px] text-gray-500/90 dark:text-gray-400 md:text-base">
+              © NEUROFUSION Research Inc. All rights reserved. {new Date().getFullYear()}
+            </span>
+          </p>
+          <nav className="my-6 flex flex-wrap justify-center" aria-label="Footer">
+            {footerLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={`${link.href}`}
+                className="ml-6 text-[14.5px] font-normal text-gray-500 opacity-90 hover:text-primary-900 hover:opacity-100 dark:text-primary-50 dark:hover:text-white"
+                target={link.external ? "_blank" : undefined}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </nav>
+        </div>
       </div>
     </footer>
   );
