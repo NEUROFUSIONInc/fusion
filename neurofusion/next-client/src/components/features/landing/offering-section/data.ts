33c3d595f63b3fcf3c530839422ff479fd1f3a94
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-20 20:41:06 +0100

    feat: create offering section and a set of disclaimers for our product

---
+++ neurofusion/next-client/src/components/features/landing/offering-section/data.ts
@@ -0,0 +1,14 @@
+import { Hand, Lock } from "lucide-react";
+
+export const fusionOfferingFeatures = [
+  {
+    icon: Lock,
+    title: "Data privacy",
+    description: "Prompts & responses are stored on your device. We anonymise and encrypt data from other sources.",
+  },
+  {
+    icon: Hand,
+    title: "No spam",
+    description: "We don't sell your data, and we don't spam you.",
+  },
+];
