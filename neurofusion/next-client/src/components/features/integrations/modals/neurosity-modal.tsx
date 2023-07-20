cf4907d3676ebdb571906dc210f4d549eadeb19b
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-20 20:26:30 +0100

    feat: update neurosity modal to use appropriate color styles

--- neurofusion/next-client/src/components/features/integrations/modals/neurosity-modal.tsx
+++ neurofusion/next-client/src/components/features/integrations/modals/neurosity-modal.tsx
@@ -47,7 +47,7 @@ export const NeurosityModal: FC<INeurosityModalProps> = ({ isOpen, onCloseModal 
             Select an option
             <select
               id="countries"
-              className="focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-indigo-500 dark:focus:ring-indigo-500"
               onChange={updateNeurositySelectedDevice}
             >
               <option value="">Select a device</option>
