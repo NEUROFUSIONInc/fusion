9c4205f030c3e8d176a2b135f719475932d4e0bf
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-21 21:16:55 +0100

    feat: replace celebration badge svg with png

--- neurofusion/next-client/src/components/ui/layouts/footer/ms-partner.tsx
+++ neurofusion/next-client/src/components/ui/layouts/footer/ms-partner.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export const MsPartner = () => {
   return (
-    <div className="flex flex-col items-center justify-center space-y-0 md:flex-row">
+    <div className="flex flex-col items-center justify-center space-y-2 md:flex-row">
       <Image
-        src="/images/celebration-badge.svg"
+        src="/images/celebration-badge.png"
         alt="Microsoft Partner Celebration Badge"
-        width={220}
-        height={90}
+        width={786}
+        height={336}
         loading="eager"
-        className="h-16 w-48 md:h-24 md:w-56"
+        className="w-28 md:w-40"
       />
-      <p className="ml-0 text-sm font-normal text-gray-500 dark:text-gray-400 md:-ml-10 md:text-base">
+      <p className="ml-0 text-sm font-normal text-gray-500 dark:text-gray-400 md:ml-4 md:text-base">
         Proud to partner with <span className="font-medium text-dark dark:text-white">Microsoft for Startups</span>
       </p>
     </div>
