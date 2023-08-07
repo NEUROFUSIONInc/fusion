908e792eac22ce9aa0ff8fbe60f9201f9992ff2d
Ore Ogundipe <oreogundipe@gmail.com>
2023-08-08 00:28:46 +0100

    build: latest ios version for submission

--- fusionMobile/app.config.ts
+++ fusionMobile/app.config.ts
@@ -20,7 +20,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.neurofusion.fusion",
-    buildNumber: "38",
+    buildNumber: "44",
     backgroundColor: "#0B0816",
   },
   android: {
