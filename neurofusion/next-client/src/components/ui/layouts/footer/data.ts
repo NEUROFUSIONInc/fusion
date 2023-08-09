c3219ab309023cf4b8f92eb68a75faf2d3e94e58
Ore O <oreogundipe@gmail.com>
2023-08-09 18:53:47 +0100

    feat: add privacy policy page (#138)

--- neurofusion/next-client/src/components/ui/layouts/footer/data.ts
+++ neurofusion/next-client/src/components/ui/layouts/footer/data.ts
@@ -6,8 +6,7 @@ export const footerLinks = [
   },
   {
     title: "Privacy Policy",
-    href: "https://island-manicure-a10.notion.site/Fusion-Data-Use-Privacy-Policy-e6f36d7cf99e4c1180bcafdece020e73",
-    external: true,
+    href: "/privacy",
   },
   {
     title: "Contact",
