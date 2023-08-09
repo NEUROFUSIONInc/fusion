27ad97b1decb275fec578b125d58065955a340d1
Ore Ogundipe <oreogundipe@gmail.com>
2023-08-09 18:48:58 +0100

    feat: add privacy policy page

---
+++ neurofusion/next-client/src/pages/privacy.tsx
@@ -0,0 +1,160 @@
+import type { NextPage } from "next";
+
+import { MainLayout } from "~/components/layouts";
+
+const Privacy: NextPage = () => {
+  return (
+    <MainLayout>
+      <div className="flex justify-center flex-col text-center mt-20 mb-20">
+        <div>
+          <h1>Privacy Policy</h1>
+          <p>Last updated: August 9, 2023</p>
+        </div>
+        <div className="ml-20 mr-20">
+          <p>
+            This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your
+            information when You use the Service and tells You about Your privacy rights and how the law protects You.
+          </p>
+          <p>
+            NEUROFUSION Research, Inc. (the “Company”) is committed to maintaining robust privacy protections for its
+            users. Our Privacy Policy (“Privacy Policy”) is designed to help you understand how we collect, use and
+            safeguard the information you provide to us and to assist you in making informed decisions when using our
+            Service. For purposes of this Agreement, “Site” refers to the Company’s website, which can be accessed at
+            https://usefusion.app or through our mobile application. “Service” refers to the Company’s services accessed
+            via the Site, in which users can track changes in their behavior over time using personalized prompts. The
+            terms “we,” “us,” and “our” refer to the Company. “You” refers to you, as a user of our Site or our Service.
+            By accessing our Site or our Service, you accept our Privacy Policy and Terms of Use (found here:
+            https://usefusion.app/terms-of-use), and you consent to our collection, storage, use and disclosure of your
+            Personal Information as described in this Privacy Policy.
+          </p>
+          <br />
+          <h2>I. INFORMATION WE COLLECT</h2>
+          <p>
+            We collect “Non-Personal Information” and “Personal Information.” Non-Personal Information includes
+            information that cannot be used to personally identify you, such as anonymous usage data, general
+            demographic information we may collect, referring/exit pages and URLs, platform types, preferences you
+            submit and preferences that are generated based on the data you submit and number of clicks. Personal
+            Information includes your email which is only used to contact you & not linked to your non-personal
+            information.
+          </p>
+          <h3>1. Information collected via Technology</h3>
+          <p>
+            To activate the Service you do not need to submit any personal Information. To use the Service thereafter,
+            you do not need to submit further Personal Information. However, in an effort to improve the quality of the
+            Service, we track information provided to us by your browser or by our software application when you view or
+            use the Service, such as the website you came from (known as the “referring URL”), the type of browser you
+            use, the device from which you connected to the Service, the time and date of access, and other information
+            that does not personally identify you. We track this information using cookies, or small text files which
+            include an anonymous unique identifier. Cookies are sent to a user’s browser from our servers and are stored
+            on the user’s computer hard drive. Sending a cookie to a user’s browser enables us to collect Non-Personal
+            information about that user and keep a record of the user’s preferences when utilizing our services, both on
+            an individual and aggregate basis. The Company may use both persistent and session cookies; persistent
+            cookies remain on your computer after you close your session and until you delete them, while session
+            cookies expire when you close your browser.{" "}
+          </p>
+          <h3>2. Information you provide us by registering for an account</h3>
+          <p>
+            In addition to the information provided automatically by your browser when you visit the Site, to become a
+            subscriber to the Service an anonymous profile is created for you using the decentralized identity protocol
+            Nostr https://nostr.com/. You can optionally submit your email address for the purpose of us contacting you.
+            By doing this, you are authorizing us to collect, store and use your email address in accordance with this
+            Privacy Policy.{" "}
+          </p>
+
+          <h3>3. Children’s Privacy </h3>
+          <p>
+            The Site and the Service are not directed to anyone under the age of 13. The Site does not knowingly collect
+            or solicit information from anyone under the age of 13, or allow anyone under the age of 13 to sign up for
+            the Service. In the event that we learn that we have gathered personal information from anyone under the age
+            of 13 without the consent of a parent or guardian, we will delete that information as soon as possible. If
+            you believe we have collected such information, please contact us at contact@usefusion.app.
+          </p>
+
+          <br />
+          <h2>II. HOW WE USE AND SHARE INFORMATION</h2>
+          <h3>Personal Information:</h3>
+          <p>
+            Except as otherwise stated in this Privacy Policy, we do not sell, trade, rent or otherwise share for
+            marketing purposes your Personal Information with third parties without your consent. We do share Personal
+            Information with vendors who are performing services for the Company, such as the servers for our email
+            communications who are provided access to user’s email address for purposes of sending emails from us. Those
+            vendors use your Personal Information only at our direction and in accordance with our Privacy Policy. In
+            general, the Personal Information you provide to us is used to help us communicate with you. For example, we
+            use Personal Information to contact users in response to questions, solicit feedback from users, provide
+            technical support, and inform users about promotional offers. We may share Personal Information with outside
+            parties if we have a good-faith belief that access, use, preservation or disclosure of the information is
+            reasonably necessary to meet any applicable legal process or enforceable governmental request; to enforce
+            applicable Terms of Service, including investigation of potential violations; address fraud, security or
+            technical concerns; or to protect against harm to the rights, property, or safety of our users or the public
+            as required or permitted by law. Non-Personal Information In general, we use Non-Personal Information to
+            help us improve the Service and customize the user experience. We also aggregate Non-Personal Information in
+            order to track trends and analyze use patterns on the Site. This Privacy Policy does not limit in any way
+            our use or disclosure of Non-Personal Information, we will only share information with third parties per
+            your request. An example of this is should you choose to participate in a study via the platform, the
+            organizer will request for you to share your data and we will facilitate the sharing between you and them.
+            In the event we undergo a business transaction such as a merger, acquisition by another company, or sale of
+            all or a portion of our assets, your Personal Information may be among the assets transferred. You
+            acknowledge and consent that such transfers may occur and are permitted by this Privacy Policy, and that any
+            acquirer of our assets may continue to process your Personal Information as set forth in this Privacy
+            Policy. If our information practices change at any time in the future, we will post the policy changes to
+            the Site so that you may opt out of the new information practices. We suggest that you check the Site
+            periodically if you are concerned about how your information is used.
+          </p>
+          <br />
+          <h2>III. HOW WE PROTECT INFORMATION</h2>
+          <p>
+            We implement security measures designed to protect your information from unauthorized access. Your account
+            is protected by your private key and we urge you to take steps to keep your personal information safe by not
+            disclosing it. We further protect your information from potential security breaches by implementing certain
+            technological security measures including encryption, firewalls and secure socket layer technology. As your
+            information such as prompt text & responses are stored only on your device, an attacker will need to gain
+            physical access to it to see your data. By using our Service, you acknowledge that you understand and agree
+            to assume these risks.
+          </p>
+          <br />
+          <h2>IV. YOUR RIGHTS REGARDING THE USE OF YOUR PERSONAL INFORMATION</h2>
+          <p>
+            You have the right at any time to prevent us from contacting you for marketing purposes. When we send a
+            promotional communication to a user, the user can opt out of further promotional communications by following
+            the unsubscribe instructions provided in each promotional e-mail. You can also indicate that you do not wish
+            to receive marketing communications from us in the settings section of the Site. Please note that
+            notwithstanding the promotional preferences you indicate by either unsubscribing or opting out in the
+            settings page of the Site, we may continue to send you administrative emails including, for example,
+            periodic updates to our Privacy Policy.
+          </p>
+          <br />
+          <h2> V. LINKS TO OTHER WEBSITES </h2>
+          <p>
+            As part of the Service, we may provide links to or compatibility with other websites or applications.
+            However, we are not responsible for the privacy practices employed by those websites or the information or
+            content they contain. This Privacy Policy applies solely to information collected by us through the Site and
+            the Service. Therefore, this Privacy Policy does not apply to your use of a third party website accessed by
+            selecting a link on our Site or via our Service. To the extent that you access or use the Service through or
+            on another website or application, then the privacy policy of that other website or application will apply
+            to your access or use of that site or application. We encourage our users to read the privacy statements of
+            other websites before proceeding to use them.
+          </p>
+          <br />
+          <h2>VI. CHANGES TO OUR PRIVACY POLICY </h2>
+          <p>
+            The Company reserves the right to change this policy and our Terms of Service at any time. We will notify
+            you of significant changes to our Privacy Policy by sending a notice to the primary email address specified
+            in your account or by placing a prominent notice on our site. Significant changes will go into effect 30
+            days following such notification. Non-material changes or clarifications will take effect immediately. You
+            should periodically check the Site and this privacy page for updates.
+          </p>
+          <br />
+          <h2>VII. CONTACT US</h2>
+          <p>
+            If you have any questions regarding this Privacy Policy or the practices of this Site, please contact us by
+            sending an email to contact@usefusion.app.
+          </p>
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+(Privacy as any).theme = "light";
+
+export default Privacy;
