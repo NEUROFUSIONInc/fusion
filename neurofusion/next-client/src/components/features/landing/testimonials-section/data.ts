ee678d99affe307bb15bceaa0fd1f949eb29b47c
Ore O <oreogundipe@gmail.com>
2023-08-13 19:07:28 +0100

    copy: include sabby feedback

--- neurofusion/next-client/src/components/features/landing/testimonials-section/data.ts
+++ neurofusion/next-client/src/components/features/landing/testimonials-section/data.ts
@@ -9,4 +9,12 @@ export const testimonials: Testimonial[] = [
       "Fusion has been incredible in navigating my daily routine. This app has significantly improved my habits, especially when it comes to taking my medications, managing stress, and staying aware of my overall health. With various prompts set at different intervals, it ensures that I never miss a beat in my self-care journey.",
     image: "/images/testimonials/deji.jpeg",
   },
+  {
+    id: 2,
+    name: "Sabby A.",
+    location: "Vancouver, BC",
+    highlight:
+      "It's helped me track my eating habits, exercise routine, and progress towards my personal goals 💪🏽",
+    image: "/images/testimonials/sabby.jpeg",
+  },
 ];
