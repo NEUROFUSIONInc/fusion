1fba6487862f8321c66c7bd58307eeda691b6e7a
Ore Ogundipe <oreogundipe@gmail.com>
2023-09-10 19:20:10 +0100

    feat: quest page ui

--- neurofusion/next-client/src/pages/quests.tsx
+++ neurofusion/next-client/src/pages/quests.tsx
@@ -3,14 +3,50 @@ import { getServerSession } from "next-auth";
 import React from "react";
 
 import { authOptions } from "./api/auth/[...nextauth]";
-import { DashboardLayout } from "~/components/layouts";
+import { DashboardLayout, Meta } from "~/components/layouts";
+import { Button, Input } from "~/components/ui";
 
 const QuestsPage: NextPage = () => {
+  const [recepientNpubs, setRecepientNpubs] = React.useState<string>("");
+  const [promptConfig, setPromptConfig] = React.useState<string>("");
   return (
     <DashboardLayout>
+      <Meta
+        meta={{
+          title: "Fusion | Quests",
+        }}
+      />
       <div className="flex flex-col items-center justify-center w-full h-full">
         <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-gray-100">Configure Quests</h1>
         <p className="mt-2 text-lg text-center text-gray-600 dark:text-gray-400"></p>
+
+        <>
+          <Input
+            label="Recipient Fusion IDs"
+            type="text"
+            size="lg"
+            fullWidth
+            placeholder="Enter Recipient Fusion IDs"
+            value={recepientNpubs}
+            onChange={(e) => setRecepientNpubs(e.target.value)}
+          />
+
+          <Input
+            label="Prompt Config"
+            type="text"
+            size="lg"
+            fullWidth
+            placeholder="Enter Prompt Config (JSON)"
+            value={promptConfig}
+            multiline
+            className="pt-4 h-40"
+            onChange={(e) => setPromptConfig(e.target.value)}
+          />
+
+          <Button type="submit" size="lg" fullWidth className="mt-4">
+            Deploy
+          </Button>
+        </>
       </div>
     </DashboardLayout>
   );
