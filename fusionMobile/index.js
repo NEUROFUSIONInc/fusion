a868b63b242cedc950749069a0008e62279dbe6d
Anthony Nwaizuzu <tonychuks9@live.com>
2023-09-14 10:52:37 +0100

    fix: moved basetable to the index file (#151)

--- fusionMobile/index.js
+++ fusionMobile/index.js
@@ -1,11 +1,20 @@
 import "./globals.js";
 import { registerRootComponent } from "expo";
 import React from "react";
+import { Alert } from "react-native";
 
 import App from "./App.tsx";
 import { NavigationContainer } from "./src/navigation/navigation-container.tsx";
 
 import { AccountContextProvider } from "~/contexts/account.context.tsx";
+import { createBaseTables } from "~/lib";
+
+(async () => {
+  const setupStatus = await createBaseTables();
+  if (!setupStatus) {
+    Alert.alert("Error", "There was an error setting up the app.");
+  }
+})();
 
 const MainApp = () => {
   return (
