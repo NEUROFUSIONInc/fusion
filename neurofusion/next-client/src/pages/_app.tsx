83764bd3652d61f375b60e54a907809bc0f7210a
Ore Ogundipe <oreogundipe@gmail.com>
2023-09-14 11:55:04 +0100

    feat: add microsoft clarity

--- neurofusion/next-client/src/pages/_app.tsx
+++ neurofusion/next-client/src/pages/_app.tsx
@@ -8,12 +8,23 @@ import { QUERY_OPTIONS_DEFAULT } from "~/config";
 import "../styles/globals.css";
 import { gtw } from "~/utils";
 import { Meta } from "~/components/layouts";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient({ defaultOptions: QUERY_OPTIONS_DEFAULT }));
 
   return (
     <>
+      <Head>
+        <script type="text/javascript">
+          {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "h7a11p7zxo");`}
+        </script>
+      </Head>
       <Meta />
       <SessionProvider session={session}>
         <QueryClientProvider client={queryClient}>
