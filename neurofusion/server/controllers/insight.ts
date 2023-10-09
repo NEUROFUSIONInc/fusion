fe5eabe4fcc2c84edfaf55b39558a369fc2af726
Ore Ogundipe <oreogundipe@gmail.com>
2023-10-09 08:54:08 -0700

    disable server logs

--- neurofusion/server/controllers/insight.ts
+++ neurofusion/server/controllers/insight.ts
@@ -9,7 +9,6 @@ const openai = new OpenAI({
 export async function getPromptSummary(req: any, res: any) {
   // prompt
   // responses
-  console.log(req.body.prompt);
   const prompt: Prompt = req.body.prompt;
   const responses: PromptResponse[] = req.body.responses;
 
@@ -28,40 +27,56 @@ export async function getPromptSummary(req: any, res: any) {
   }
 
   const timePeriod = "week";
-  const chatCompletions = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: `You are a helpful assistant that for an Fusion that helps people manage & improve their wellbeing.
-          We look at wellbeing holistically across the following categories - Mental Health, Productivity, Relationships, Health and Fitness, Spiritual Practice, Self Care, Finance, Personal Interest, Other.
-          Please provide a concise summary of trends and insights for the '${prompt.additionalMeta?.category}' category in the past ${timePeriod} to the user.
-          Include any notable changes in responses and suggest actionable steps for the user. Be curious, not judgemental.
+  try {
+    const chatCompletions = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `You are a helpful assistant that for an Fusion that helps people manage & improve their wellbeing.
+            We look at wellbeing holistically across the following categories - Mental Health, Productivity, Relationships, Health and Fitness, Spiritual Practice, Self Care, Finance, Personal Interest, Other.
+            Please provide a concise summary of trends and insights for the '${prompt.additionalMeta?.category}' category in the past ${timePeriod} to the user.
+            Include any notable changes in responses and suggest actionable steps for the user. Be curious, not judgemental.
+            
+            YOU MUST ADHERE TO THE FOLLOWING WHEN RESPONDING:
+            - YOU ARE RESPONDING TO THE USER. YOU MUST USE "YOU" AND "YOUR" IN YOUR RESPONSE. NEVER "THE USER" OR "THEY".\n
+            - Your answers are very concise and straight to the point. Maximum 500 characters.\n
+            - Ensure that the analysis is data-driven and based on the most recent data.\n
+            - Your response will directly be sent to the user so change your language accordingly. \n
+            - YOUR ANSWERS ARE BASED ON DATA AND DATE. DO NOT SAY "YOUR HEART RATE IS 55" WHEN IT WAS YESTERDAY DATA FOR EXAMPLE.
+            `,
+        },
+        {
+          role: "user",
+          content: `These are responses my responses to the prompt '${prompt.promptText}'.\n\n
           
-          YOU MUST ADHERE TO THE FOLLOWING WHEN RESPONDING:
-          - YOU ARE RESPONDING TO THE USER. YOU MUST USE "YOU" AND "YOUR" IN YOUR RESPONSE. NEVER "THE USER" OR "THEY".\n
-          - Your answers are very concise and straight to the point. Maximum 500 characters.\n
-          - Ensure that the analysis is data-driven and based on the most recent data.\n
-          - Your response will directly be sent to the user so change your language accordingly. \n
-          - YOUR ANSWERS ARE BASED ON DATA AND DATE. DO NOT SAY "YOUR HEART RATE IS 55" WHEN IT WAS YESTERDAY DATA FOR EXAMPLE.
+          ${prompt_responses}
           `,
-      },
-      {
-        role: "user",
-        content: `These are responses my responses to the prompt '${prompt.promptText}'.\n\n
-        
-        ${prompt_responses}
-        `,
-      },
-    ],
-  });
-  // If additional data is needed for a more comprehensive analysis, kindly suggest relevant prompts to gather that information.
+        },
+      ],
+    });
 
-  console.log(chatCompletions.choices[0]);
+    return res.status(200).json({
+      status: "success",
+      summary: chatCompletions.choices[0].message.content,
+      recommendation: "",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: "error",
+      message: "Something went wrong. Please try again later.",
+    });
+  }
+}
 
-  return res.status(200).json({
-    status: "success",
-    summary: chatCompletions.choices[0].message.content,
-    recommendation: "",
-  });
+export async function getPromptSuggestions(req: any, res: any) {
+  /**
+   * Takes a string and suggests prompts for user to add to their device
+   */
+  // prompt
+  // responses
+  console.log(req.body.text);
+  const prompt: Prompt = req.body.prompt;
+  const responses: PromptResponse[] = req.body.responses;
 }
