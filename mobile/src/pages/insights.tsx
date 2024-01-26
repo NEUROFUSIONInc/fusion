78990fa4af523f275448a5d551b733f883f095e0
Ore Ogundipe <oreogundipe@gmail.com>
2024-01-25 16:10:26 -0800

    feat: new ui for the insights page that lists all active prompts

--- mobile/src/pages/insights.tsx
+++ mobile/src/pages/insights.tsx
@@ -1,4 +1,5 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
+import { cva } from "class-variance-authority";
 import dayjs from "dayjs";
 import React, {
   useCallback,
@@ -7,29 +8,28 @@ import React, {
   useMemo,
   useState,
 } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import {
   State,
   PanGestureHandler,
   ScrollView,
 } from "react-native-gesture-handler";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-import { Prompt } from "~/@types";
 import {
   Screen,
   ChartContainer,
-  Select,
   Button,
   Plus,
   CategoryTag,
   Pencil,
+  Calendar,
 } from "~/components";
+import { categories } from "~/config";
 import { AccountContext, InsightContext } from "~/contexts";
 import { usePromptsQuery } from "~/hooks";
 import { RouteProp } from "~/navigation";
 import { colors } from "~/theme";
-import { categories } from "~/config";
-
 import { appInsights, maskPromptId } from "~/utils";
 
 export function InsightsScreen() {
@@ -37,7 +37,7 @@ export function InsightsScreen() {
   const navigation = useNavigation();
 
   const insightContext = useContext(InsightContext);
-  let routePromptUuid = route.params?.promptUuid;
+  const routePromptUuid = route.params?.promptUuid;
 
   const accountContext = React.useContext(AccountContext);
 
@@ -115,23 +115,93 @@ export function InsightsScreen() {
     }
   };
 
-  /**
-   * Ugly work around because react native fails to re-render the dropdown
-   * when saved prompt is updated.
-   *
-   * Tried different approaches open to something better :)
-   */
-  const [renderDropdown, setRenderDropdown] = React.useState<boolean>(false);
+  const [displayedDates, setDisplayedDates] = useState<dayjs.Dayjs[]>([]);
   useEffect(() => {
-    if (savedPrompts) {
-      setRenderDropdown(false);
+    (async () => {
+      if (chartStartDate && insightContext!.insightPeriod === "week") {
+        const dates = await getWeeksInMonth(chartStartDate);
+        setDisplayedDates(dates);
+      }
+      if (chartStartDate && insightContext!.insightPeriod === "month") {
+        const dates = await getLastFourMonths(chartStartDate);
+        setDisplayedDates(dates);
+      }
+    })();
+  }, [chartStartDate]);
+
+  const getWeeksInMonth = (selectedDate: dayjs.Dayjs) => {
+    /**
+     * returns an array of dayjs objects that represent the start
+     * of each week in the month
+     */
+    return new Promise<dayjs.Dayjs[]>((resolve) => {
+      const firstDay = selectedDate.startOf("month");
+      // if the selected date is in the past, then the last day is the end of the month
+
+      // have to check if it's the current month... then don't
+      let lastDay;
+      if (selectedDate.get("month") === dayjs().get("month")) {
+        lastDay = dayjs().endOf("week");
+      } else if (selectedDate < dayjs().startOf("day")) {
+        lastDay = selectedDate.endOf("month").subtract(1, "day");
+      } else {
+        lastDay = dayjs().startOf("day");
+      }
+
+      const weeks = [];
+
+      let weekIterator = firstDay.startOf("week");
+      while (weekIterator.endOf("week") <= lastDay.endOf("week")) {
+        weeks.push(weekIterator);
+        weekIterator = weekIterator.add(1, "week"); // Cloning and adding 1 week
+      }
+      resolve(weeks);
+    });
+  };
+
+  const getLastFourMonths = (selectedDate: dayjs.Dayjs) => {
+    /**
+     * returns an array of dayjs objects that represent the start
+     * of each month in the last 4 months
+     */
+    return new Promise<dayjs.Dayjs[]>((resolve) => {
+      const firstDay = selectedDate.startOf("month").subtract(3, "month");
+      const lastDay = dayjs().startOf("day");
+
+      const months = [];
+
+      let monthIterator = firstDay.startOf("month");
+      while (monthIterator.endOf("month") <= lastDay.endOf("month")) {
+        months.push(monthIterator);
+        monthIterator = monthIterator.add(1, "month"); // Cloning and adding 1 month
+      }
+      resolve(months);
+    });
+  };
+
+  const dateTextStyle = cva("text-base font-sans text-gray-400 p-2", {
+    variants: {
+      active: {
+        true: "text-white",
+      },
+    },
+  });
+
+  const [isDatePickerVisible, setDatePickerVisibility] = React.useState(false);
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+  const handleDatePickerConfimm = (date: Date) => {
+    if (insightContext!.insightPeriod === "week") {
+      setChartStartDate(dayjs(date));
+    } else if (insightContext!.insightPeriod === "month") {
+      setChartStartDate(dayjs(date).startOf("month"));
     }
-  }, [savedPrompts]);
-  useEffect(() => {
-    if (renderDropdown === false) {
-      setRenderDropdown(true);
-    }
-  }, [renderDropdown]);
+    hideDatePicker();
+  };
 
   return (
     <Screen>
@@ -154,7 +224,7 @@ export function InsightsScreen() {
 
       <ScrollView
         horizontal
-        className="flex gap-x-3 gap-y-3 pl-2"
+        className="flex gap-x-3 flex-shrink-0 gap-y-3 pl-2 max-h-[100%]"
         showsHorizontalScrollIndicator={false}
       >
         {categoryPillsToDisplay.map((category) => {
@@ -172,46 +242,101 @@ export function InsightsScreen() {
           );
         })}
       </ScrollView>
+
+      <View className="flex flex-row border-b-[1px] my-5 border-tint">
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          maximumDate={dayjs().toDate()}
+          minuteInterval={5}
+          date={chartStartDate.toDate()} // TODO: revisit this
+          onConfirm={handleDatePickerConfimm}
+          onCancel={hideDatePicker}
+        />
+        <Button
+          variant="ghost"
+          size="icon"
+          leftIcon={<Calendar width={20} height={20} />}
+          onPress={showDatePicker}
+        />
+        <ScrollView
+          horizontal
+          className="flex flex-1 flex-row"
+          showsHorizontalScrollIndicator={false}
+        >
+          {displayedDates.map((date, index) => (
+            <Pressable
+              onPress={() => {
+                setChartStartDate(date.startOf(insightContext!.insightPeriod));
+              }}
+            >
+              <Text
+                key={index}
+                className={dateTextStyle(
+                  date
+                    .startOf(insightContext!.insightPeriod)
+                    .isSame(
+                      chartStartDate.startOf(insightContext!.insightPeriod)
+                    )
+                    ? { active: true }
+                    : {}
+                )}
+              >
+                {insightContext!.insightPeriod === "week" && (
+                  <>
+                    {date.get("month") !== date.endOf("week").get("month") ? (
+                      <>
+                        {date.format("MMM D")}-
+                        {date.endOf("week").format("MMM D")}
+                      </>
+                    ) : (
+                      <>
+                        {date.format("MMM D")}-{date.endOf("week").format("D")}
+                      </>
+                    )}
+                  </>
+                )}
+
+                {insightContext!.insightPeriod === "month" && (
+                  <>{date.format("MMM YYYY")}</>
+                )}
+              </Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+      </View>
+
       {savedPrompts && savedPrompts.length > 0 && (
         <PanGestureHandler onHandlerStateChange={onHandlerStateChange}>
-          <ScrollView nestedScrollEnabled>
-            {/* now add the date selectors */}
-            <View className="flex flex-1 flex-row border-b-[1px] mt-5 mb-5 m-2 border-tint">
-              {insightContext!.insightPeriod === "month" && (
-                // list all the months in the year
-                <Text className="text-base text-white p-2">
-                  {chartStartDate.format("MMM YYYY")}
-                </Text>
-              )}
-            </View>
-
-            {/* select the first available prompt */}
+          <ScrollView nestedScrollEnabled showsVerticalScrollIndicator={false}>
             <View className="flex flex-1 flex-col w-full h-auto justify-between">
               {filteredPrompts?.map((prompt) => (
-                <View
-                  key={prompt.uuid}
-                  className="rounded-lg bg-secondary-900 mb-10"
-                >
-                  <View className="flex flex-row border-b-[1px] border-tint p-5 justify-between">
-                    <Text className="text-base font-sans text-white leading-10 max-w-[90%]">
-                      {prompt.promptText}
-                    </Text>
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      leftIcon={<Pencil width={20} height={20} />}
-                      onPress={() => {}}
-                    />
-                  </View>
+                <>
+                  <View
+                    key={prompt.uuid}
+                    className="rounded-lg bg-secondary-900 mb-10"
+                  >
+                    <View className="flex flex-row border-b-[1px] border-tint p-5 justify-between">
+                      <Text className="text-lg font-sans text-white max-w-[90%]">
+                        {prompt.promptText}
+                      </Text>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        leftIcon={<Pencil width={25} height={30} />}
+                        onPress={() => {}}
+                      />
+                    </View>
 
-                  <View>
-                    <ChartContainer
-                      prompt={prompt}
-                      startDate={chartStartDate}
-                      timePeriod={insightContext!.insightPeriod}
-                    />
+                    <View>
+                      <ChartContainer
+                        prompt={prompt}
+                        startDate={chartStartDate}
+                        timePeriod={insightContext!.insightPeriod}
+                      />
+                    </View>
                   </View>
-                </View>
+                </>
               ))}
             </View>
           </ScrollView>
