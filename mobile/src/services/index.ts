6cacb4930958747ea6a08458150d1db98dbfae10
Ore Ogundipe <oreogundipe@gmail.com>
2024-06-06 06:05:28 -0700

    feat: setup background sync for active user quests

--- mobile/src/services/index.ts
+++ mobile/src/services/index.ts
@@ -2,3 +2,5 @@ export * from "./notification.service";
 export * from "./prompt.service";
 export * from "./nostr.service";
 export * from "./streaks.service";
+export * from "./quest.service";
+export * from "./taskmanager.service";
