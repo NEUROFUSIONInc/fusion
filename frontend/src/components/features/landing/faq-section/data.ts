567c20803a113de048ed7f15046a55e7bc564ec9
Ore Ogundipe <oreogundipe@gmail.com>
2024-07-03 17:26:03 -0700

    add: update faqs

--- frontend/src/components/features/landing/faq-section/data.ts
+++ frontend/src/components/features/landing/faq-section/data.ts
@@ -3,34 +3,35 @@ export const frequentlyAskedQuestions = [
     id: 1,
     question: "What is Fusion, and how does it work?",
     answer:
-      "Fusion is a mobile application that helps you proactively manage your physical health, emotional shifts, productivity and overall wellbeing. We do this by providing personalized prompts, actionable recommendations, and insights to help you understand and navigate your days better.",
+      "Fusion is a mobile application that helps researchers and explorers design experiments for research and deploy them remotely to participants. It provides a collaborative platform for engaging experiments, instant data analysis, and designing custom experiments.",
   },
-  {
-    id: 2,
-    question: "Why should I use Fusion?",
-    answer:
-      "You can use Fusion if you are looking to;\nGet a deeper understanding of your health, productivity and well-being.\n Live your days with more agency and less guesswork.\n Receive personalized recommendations into areas that matter to you.\n Access a privacy-conscious community for decentralized research.\n Contribute to advancements in health and wellness research.",
-  },
+  // {
+  //   id: 2,
+  //   question: "Why should I use Fusion?",
+  //   answer:
+  //     "You should Fusion if you are looking to;\n\n Collaborate with fellow researchers and explorers.\n Design and conduct custom experiments tailored to your research goals in minutes\n Reach any one with supported integrations remotely.\n Get instant analysis from recordings & raw data. \nTrack adherence of protocols from participants in real time.\nPublish your research results and make them accessible for others to run with their data.",
+  // },
   {
     id: 3,
     question: "I only have a smartphone. Can I use Fusion?",
-    answer: "Yes, you can! Fusion is available for download on the iOS and Google Playstore for free.",
+    answer:
+      "Yes, you can! In the Fusion mobile app, you can monitor changes in your behavior and health with personalized recommendations. The app is designed to be user-friendly and accessible, ensuring that you can take full advantage of its features using just your smartphone.",
   },
   // {
   //   id: 4,
   //   question: "How much does fusion cost?",
   //   answer:
-  //     "You can access Fusion for free to get personalized prompts and insights. Our premium features such as summaries of your insights and recommendations are available for just $9.99/month.",
+  //     "You can purchase research credits do deploy studies starting at $9.99. We charge $1 for each participant reached.",
   // },
   // {
   //   id: 5,
   //   question: "Is there a free trial?",
   //   answer:
-  //     "Setting and responding to prompts will always be free. We offer a 3 day trial of Fusion Premium for intelligent recommendations and more exciting features.",
+  //     "Setting and responding to prompts will always be free.",
   // },
   {
     id: 6,
-    question: "Can I collaborate with healthcare providers or researchers using Fusion?",
+    question: "I want to join an active research study?",
     answer:
       "Absolutely. Fusion allows seamless communication and collaboration with healthcare providers and researchers. You can securely share your data and receive personalized interventions and recommendations remotely.",
   },
@@ -38,7 +39,7 @@ export const frequentlyAskedQuestions = [
     id: 7,
     question: "How secure is my information?",
     answer:
-      "Your data privacy is our priority. We store your prompts and responses directly on your mobile device. Your data is entirely personal, and no one, not even us at Fusion, can access it without your explicit permission.",
+      "Your data privacy is our priority. Your research data is stored directly on your device. Your data is entirely personal, and no one, not even us at Fusion, can access it without your explicit permission. When you join a quest, you agree to sharing your data with the organizers.",
   },
 ];
 
@@ -79,12 +80,12 @@ export const faqsExplorersResearchers = [
     answer:
       "Your data privacy is our priority. Your research data is stored directly on your device. Your data is entirely personal, and no one, not even us at Fusion, can access it without your explicit permission.",
   },
-  {
-    id: 7,
-    question: "How can I collaborate with healthcare providers or researchers using Fusion?",
-    answer:
-      "Absolutely. Fusion allows seamless communication and collaboration with healthcare providers and researchers. You can securely share your data and receive personalized interventions and recommendations remotely.",
-  },
+  // {
+  //   id: 7,
+  //   question: "How can I collaborate with healthcare providers or researchers using Fusion?",
+  //   answer:
+  //     "Absolutely. Fusion allows seamless communication and collaboration with healthcare providers and researchers. You can securely share your data and receive personalized interventions and recommendations remotely.",
+  // },
   {
     id: 8,
     question: "How can I provide feedback or get additional support?",
