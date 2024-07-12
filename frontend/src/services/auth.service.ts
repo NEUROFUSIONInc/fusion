e7991274e0f9a35d9c0c8ded1e7cc2ddb3f0a98d
Ore Ogundipe <oreogundipe@gmail.com>
2024-07-12 10:54:35 -0700

    fix: update type cast for window.nostr to work

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -36,10 +36,9 @@ class AuthService {
       const authToken: string = await (async () => {
         return new Promise((resolve) => {
           sub.on("event", async (event) => {
-            // @ts-ignore
             const decoded = privateKey
               ? await nip04.decrypt(privateKey, serverPublicKey!, event.content)
-              : await window.nostr?.nip04.decrypt(serverPublicKey!, event.content);
+              : await (window as any).nostr?.nip04.decrypt(serverPublicKey!, event.content);
             resolve(decoded);
           });
         });
